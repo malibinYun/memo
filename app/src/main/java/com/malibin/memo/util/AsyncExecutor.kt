530e091e8e45e586a75b105d3b91c9f7039e1eeb
Malibin <nightmare73@naver.com>
2020-02-12 02:09:01 +0900

    [modify] rxJava 코드 가독성 문제랑 역량상 깔끔하게 짤 수 없다고 판단, 구글 아키텍쳐 참고하여 비동기 처리 객체 작성

--- app/src/main/java/com/malibin/memo/util/AsyncExecutor.kt
+++ app/src/main/java/com/malibin/memo/util/AsyncExecutor.kt
@@ -1,33 +1,29 @@
 package com.malibin.memo.util
 
-import android.annotation.SuppressLint
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
-@SuppressLint("CheckResult")
-fun <V> executeAsyncInIoThread(
-    callable: () -> V,
-    error: (t: Throwable) -> Unit,
-    callback: ((values: V) -> Unit)
-) {
-    Single.fromCallable(callable)
-        .subscribeOn(Schedulers.io())
-        .subscribe(
-            { callback.invoke(it) },
-            { error.invoke(it) })
-}
+class AsyncExecutor {
 
-@SuppressLint("CheckResult")
-fun <V> executeAsyncInMainThread(
-    callable: () -> V,
-    error: (t: Throwable) -> Unit,
-    callback: ((values: V) -> Unit)
-) {
-    Single.fromCallable(callable)
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribeOn(Schedulers.io())
-        .subscribe(
-            { callback.invoke(it) },
-            { error.invoke(it) })
+    val ioThread: Executor = IoThreadExecutor()
+    val mainThread: Executor = MainThreadExecutor()
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+
+    private class IoThreadExecutor : Executor {
+        private val ioThreadExecutor = Executors.newSingleThreadExecutor()
+
+        override fun execute(command: Runnable) {
+            ioThreadExecutor.execute(command)
+        }
+    }
+
 }
