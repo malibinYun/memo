8fc0cbec8ca0dbea93714aaada97717928568bd3
Malibin <nightmare73@naver.com>
2020-02-17 16:10:12 +0900

    [add] 카테고리 추가/수정 Activity에서 toast, success 여부 구독 코드 작성 및 viewmodel start코드 추가

--- app/src/main/java/com/malibin/memo/ui/category/AddModifyCategoryActivity.kt
+++ app/src/main/java/com/malibin/memo/ui/category/AddModifyCategoryActivity.kt
@@ -1,8 +1,11 @@
 package com.malibin.memo.ui.category
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.malibin.memo.R
 import com.malibin.memo.databinding.ActivityAddModifyCategoryBinding
@@ -15,13 +18,37 @@ class AddModifyCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val tossedCategoryId = getTossedCategoryId()
         val viewModel =
             ViewModelProvider(this, viewModelFactory)[AddModifyCategoryViewModel::class.java]
+        viewModel.start(tossedCategoryId)
+
+        subscribeSaveSuccess(viewModel)
+        subscribeToastMessage(viewModel)
 
         val binding: ActivityAddModifyCategoryBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_add_modify_category)
 
         binding.addModifyCategoryVM = viewModel
         binding.lifecycleOwner = this
+    }
+
+    private fun subscribeToastMessage(viewModel: AddModifyCategoryViewModel) {
+        viewModel.toastMessage.observe(this, Observer { stringId ->
+            Toast.makeText(this, stringId, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    private fun subscribeSaveSuccess(viewModel: AddModifyCategoryViewModel) {
+        viewModel.isSuccess.observe(this, Observer { isSuccess ->
+            if (isSuccess) {
+                setResult(Activity.RESULT_OK)
+                finish()
+            }
+        })
+    }
+
+    private fun getTossedCategoryId(): String? {
+        return intent.getStringExtra("categoryId")
     }
 }
