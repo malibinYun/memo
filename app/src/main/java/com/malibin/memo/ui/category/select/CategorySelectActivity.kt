e264c586fb7c59f49a87d85bc14aa923dd49c5aa
Malibin <nightmare73@naver.com>
2020-02-19 00:25:09 +0900

    [add] Category 선택 Activity Layout 완성

---
+++ app/src/main/java/com/malibin/memo/ui/category/select/CategorySelectActivity.kt
@@ -0,0 +1,13 @@
+package com.malibin.memo.ui.category.select
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.malibin.memo.R
+
+class CategorySelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_select)
+    }
+}
