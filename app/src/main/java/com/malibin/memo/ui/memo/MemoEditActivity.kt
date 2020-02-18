d74e8994b4ecb56ddb206fe153334af189ce7538
Malibin <nightmare73@naver.com>
2020-02-19 02:19:16 +0900

    [add] Memo Edit Activity Layout 완성

---
+++ app/src/main/java/com/malibin/memo/ui/memo/MemoEditActivity.kt
@@ -0,0 +1,13 @@
+package com.malibin.memo.ui.memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.malibin.memo.R
+
+class MemoEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_edit)
+    }
+}
