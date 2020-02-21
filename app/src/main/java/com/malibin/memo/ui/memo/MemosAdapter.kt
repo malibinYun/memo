c89c9d9e65fa7d3848af374fe7f0015b0efeeebd
Malibin <nightmare73@naver.com>
2020-02-21 13:59:49 +0900

    [add] MemosAdapter 기본 골격 완성

---
+++ app/src/main/java/com/malibin/memo/ui/memo/MemosAdapter.kt
@@ -0,0 +1,47 @@
+package com.malibin.memo.ui.memo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.malibin.memo.databinding.ItemMemoBinding
+import com.malibin.memo.db.entity.Memo
+
+class MemosAdapter(
+    private val memosViewModel: MemosViewModel
+) : ListAdapter<Memo, MemosAdapter.ViewHolder>(DiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemMemoBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val memo = getItem(position)
+        holder.bind(memo)
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemMemoBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(memo: Memo) {
+            binding.memo = memo
+            binding.memosVM = memosViewModel
+        }
+    }
+
+    private class DiffCallBack : DiffUtil.ItemCallback<Memo>() {
+        override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
